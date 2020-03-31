d67a993c31b89b769d3620c7b5432d85cca4a896
Jari Nykänen <45241047+jarinykanen@users.noreply.github.com>
2020-03-31 09:22:52 +0300

    Update src/components/right-panel-editors/event-trigger-editor.tsx
    Co-Authored-By: Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>

--- src/components/right-panel-editors/event-trigger-editor.tsx
+++ src/components/right-panel-editors/event-trigger-editor.tsx
@@ -83,7 +83,7 @@ export class EventTriggerEditor extends React.Component<Props, State> {
     if (prevProps.selectedEventTrigger !== this.props.selectedEventTrigger) {
       this.setState({
         selectedNavigationPage: undefined
-      })
+      });
     }
   }
 
