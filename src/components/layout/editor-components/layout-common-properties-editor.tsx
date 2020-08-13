650a6fa8227c0de1f6b2dc765e7afe7aceadcc34
Jari Nykänen <45241047+jarinykanen@users.noreply.github.com>
2020-08-13 22:44:55 +0300

    Update src/components/layout/editor-components/layout-common-properties-editor.tsx
    Co-authored-by: Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>

--- src/components/layout/editor-components/layout-common-properties-editor.tsx
+++ src/components/layout/editor-components/layout-common-properties-editor.tsx
@@ -77,7 +77,9 @@ class CommonLayoutPropertiesEditor extends React.Component<Props, State> {
       JSON.stringify(prevProps.pageLayout) !== JSON.stringify(pageLayout) ||
       JSON.stringify(prevProps.subLayout) !== JSON.stringify(subLayout))
     {
-      editingSubLayout ? this.setState({ layout: subLayout }) : this.setState({ layout: pageLayout });
+    this.setState({
+      layout: editingSubLayout ? subLayout : pageLayout
+    });
     }
   }
 
