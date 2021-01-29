ffea03d0b4cd542c03bbec40704c445760e3dc03
Tuomas Kämppi <33017418+Kaemppi@users.noreply.github.com>
2021-01-29 14:40:04 +0200

    Update src/components/layout/editor-components/widget-editors/tab-layout-editor.tsx
    Co-authored-by: Jari Nykänen <45241047+jarinykanen@users.noreply.github.com>

--- src/components/layout/editor-components/widget-editors/tab-layout-editor.tsx
+++ src/components/layout/editor-components/widget-editors/tab-layout-editor.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { PageLayoutViewProperty, PageLayoutViewPropertyType, PageLayoutView, PageLayout } from "../../../../generated/client";
 import strings from "../../../../localization/strings";
-import { WithStyles, withStyles, Typography, Divider, MenuItem, Select, Box } from '@material-ui/core';
+import { WithStyles, withStyles, Typography, Divider, MenuItem, Select, Box } from "@material-ui/core";
 import styles from "../../../../styles/common-properties-editor";
 import { LayoutTabPropKeys } from "../../editor-constants/keys";
 import ColorPicker from "../color-picker";
