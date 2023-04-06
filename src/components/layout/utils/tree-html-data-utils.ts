a1b536e16f639eda01a93fa576c443a708459c57
Jdallos <joshdallos@gmail.com>
2023-04-06 10:00:52 +0300

    Removed unused utils file

--- src/components/layout/utils/tree-html-data-utils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Parse HTML string to HTML
- *
- * @param html
- */
-export const parseToHtml = (htmlString: string) => {
-  return new DOMParser().parseFromString(htmlString, "text/html").body;
-};
