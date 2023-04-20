2b0c24c307b87c7c72610da673900902323e17e2
Jdallos <joshdallos@gmail.com>
2023-04-20 14:39:46 +0300

    Select for edit textComponent tag type- incomplete

--- src/components/layout/editor-components/html/text-component-properties.tsx
+++ src/components/layout/editor-components/html/text-component-properties.tsx
@@ -1,8 +1,8 @@
-import { Divider, Stack } from "@mui/material";
-import { TreeObject } from "../../../../types";
+import { Divider, MenuItem, Stack, TextField } from "@mui/material";
+import { TextComponentHTMLElementTypes, TreeObject } from "../../../../types";
 import PropertyBox from "./generic/property-box";
 import renderPanelSubtitle from "./generic/render-panel-subtitle";
-import { FC } from "react";
+import { ChangeEvent, FC } from "react";
 
 /**
  * Component props
@@ -25,9 +25,19 @@ const TextComponentProperties: FC<Props> = ({
    * @param name name
    * @param value value
    */
-  const onPropertyChange = (name: string, value: string) => {
-    component.element.style[name as any] = value;
-    updateComponent(component);
+  const onPropertyChange = ({ target: { value } } : ChangeEvent<HTMLTextAreaElement>) => {
+    console.log("property change here", value);
+
+    // TODO: Not yet implemented
+    // const updatedHTMLTag = document.createElement(value);
+    // updatedHTMLTag.innerHTML = component.element.innerHTML;
+
+    // console.log("component before", component.element.parentNode)
+    // component.element.parentNode?.replaceChild(updatedHTMLTag, component.element);
+    // // Undefined?
+    // console.log("component after", component.element.parentNode)
+
+    // updateComponent(component);
   };
 
 	return (
@@ -35,8 +45,41 @@ const TextComponentProperties: FC<Props> = ({
       <Divider sx={{ color: "#F5F5F5" }}/>
       <PropertyBox>
         { renderPanelSubtitle("text element type") }
-
-      </PropertyBox>
+          <TextField
+            value={ component.element.tagName }
+            variant="standard"
+            select
+            fullWidth
+            onChange={ onPropertyChange }
+            sx={{ backgroundColor: "#F5F5F5" }}
+            InputProps={{
+                disableUnderline: true,
+                sx: {  backgroundColor: "#F5F5F5" }
+            }}
+            SelectProps={{
+              sx: {
+                "& .MuiInputBase-input": {
+                  color: "#2196F3",
+                  height: "20px",
+                  padding: 0,
+                },
+                height: "20px",
+                backgroundColor: "#F5F5F5"
+              }
+            }}
+          >
+            { Object.values(TextComponentHTMLElementTypes).map(type => (
+              <MenuItem
+                key={ type }
+                value={ type }
+                sx={{ color: "#2196F3" }}
+              >
+                { type }
+              </MenuItem>
+            )) }
+          </TextField>
+        </PropertyBox>
+      <Divider sx={{ color: "#F5F5F5" }}/>
 		</Stack>
 	);
 };
