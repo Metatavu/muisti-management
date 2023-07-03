f5d8387e1b063150bfc674a4ae8dff68a76f2ba4
Ville Juutila <ville.juutila@gmail.com>
2023-07-03 09:25:50 +0300

    Border to add new element button for usability

--- src/components/layout/v2/layout-tree-menu-html.tsx
+++ src/components/layout/v2/layout-tree-menu-html.tsx
@@ -37,15 +37,15 @@ const LayoutTreeMenuHtml = ({
           fontWeight: 400,
           fontSize: "0.65rem",
           color: "#2196F3",
-          flexWrap: "wrap",
-          display: selectedComponent?.id === item.id ? "block" : "none"
+          display: selectedComponent?.id === item.id ? "block" : "none",
+          border: "1px dashed #2196F3",
         }}
         onClick={ () => onAddComponentClick(item.path, asChildren) }
       >
         <Stack
           direction="row"
           alignItems="center"
-          justifyContent="space-between"
+          justifyContent="space-evenly"
         >
           <AddBoxOutlined sx={{ color: "#2196F3" }}/>
           { strings.layoutEditor.addLayoutViewDialog.title }
@@ -78,6 +78,7 @@ const LayoutTreeMenuHtml = ({
           isRoot={ isRoot }
           isRootSubdirectory={ isRootSubdirectory }
           hasChildren={ hasChildren }
+          expanded={ getParentIds().includes(item.id) }
           onClick={ () => onTreeComponentSelect(item) }
           onDoubleClick={ () => {
             if (selectedComponent?.id === item.id) {
@@ -133,9 +134,7 @@ const LayoutTreeMenuHtml = ({
   };
 
   return (
-    <TreeView
-      expanded={ getParentIds() }
-    >
+    <TreeView expanded={ getParentIds() }>
       { treeObjects.map(item => renderTreeItem(item, true)) }
     </TreeView>
   );
