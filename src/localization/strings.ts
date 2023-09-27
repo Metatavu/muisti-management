324f9e46daf7cadff410dcb7af6051175b7a0066
Ville Juutila <ville.juutila@gmail.com>
2023-09-27 09:28:54 +0300

    Initial background image

--- src/localization/strings.ts
+++ src/localization/strings.ts
@@ -506,6 +506,7 @@ export interface IStrings extends LocalizedStringsMethods {
       element: string;
       elementName: string;
       proportions: string;
+      backgroundImage: string;
       videoProportionsTooltip: string;
       width: string;
       height: string;
